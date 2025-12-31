de8a5446760130dec82bb6fc61c21a41db1738b0
jzhartman <jzhartman@gmail.com>
2025-12-30 23:30:31 -0500

    Began working on use cases for adding stacks. Will want to consider renaming "Stack" type before going too far with it

---
+++ FlashCards.Application/Interfaces/IStackRepository.cs
@@ -0,0 +1,12 @@
+﻿using FlashCards.Core.Entities;
+
+namespace FlashCards.Application.Interfaces;
+
+public interface IStackRepository
+{
+    Stack GetById(int id);
+    List<Stack> GetAllStacks();
+    int Add(Stack stack);
+    void DeleteById(int id);
+    void Update();
+}
---
+++ FlashCards.Application/UseCases/CreateStackHandler.cs
@@ -0,0 +1,28 @@
+﻿using FlashCards.Application.Interfaces;
+using FlashCards.Core.Entities;
+
+namespace FlashCards.Application.UseCases;
+
+internal class CreateStackHandler
+{
+    private readonly IStackRepository _repo;
+
+    public CreateStackHandler(IStackRepository repo)
+    {
+        _repo = repo;
+    }
+
+    public int Handle(string name)
+    {
+        // Validate Name
+        var checkName = StackNameIsUnique();
+        var stack = new Stack() { Name = name };
+        return _repo.Add(stack);
+    }
+
+    private bool StackNameIsUnique()
+    {
+        // Check repo for name
+        return true;
+    }
+}
