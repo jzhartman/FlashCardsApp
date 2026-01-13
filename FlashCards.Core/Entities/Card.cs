18e22daf1a1f0f2ddd7afa7864da04954a2a714f
jzhartman <jzhartman@gmail.com>
2026-01-12 22:50:57 -0500

    Setup navigation properties in models

--- FlashCards.Core/Entities/Card.cs
+++ FlashCards.Core/Entities/Card.cs
@@ -6,6 +6,7 @@
         public int StackId { get; set; }
         public string FrontText { get; set; }
         public string BackText { get; set; }
+        public CardStack Stack { get; set; }
 
         public Card(int stackId, string frontText, string backText)
         {
