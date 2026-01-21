88daea36483896e2e9b609e0942d7eee698d18d8
jzhartman <jzhartman@gmail.com>
2026-01-20 22:16:33 -0500

    Reimplementing Result Pattern with an Error record

--- FlashCards.Core/Validation/Result.cs
+++ FlashCards.Core/Validation/Result.cs
@@ -2,20 +2,18 @@
 
 public record Result<T>
 {
-    public bool IsValid { get; }
+    public bool IsSuccess { get; }
+    public bool IsFailure => !IsSuccess;
     public T? Value { get; }
-    public List<string> Errors { get; }
+    public List<Error> Errors { get; }
 
-    private Result(bool isValid, T? value, List<string> errors)
+    private Result(bool isSuccess, T? value, List<Error> errors)
     {
-        IsValid = isValid;
+        IsSuccess = isSuccess;
         Value = value;
         Errors = errors;
     }
 
-    public static Result<T> Success(T value) => new Result<T>(true, value, new List<string>());
-
-    public static Result<T> Failure(params string[] errors) => new Result<T>(false, default, errors.ToList());
-
-    public static Result<T> Failure(IEnumerable<string> errors) => new Result<T>(false, default, errors.ToList());
+    public static Result<T> Success(T value) => new Result<T>(true, value, new List<Error>());
+    public static Result<T> Failure(params Error[] errors) => new Result<T>(false, default, errors.ToList());
 }
