c542d5288faa0a6832332fb8d5c9b4cdd50daca3
jzhartman <jzhartman@gmail.com>
2026-01-20 22:16:51 -0500

    Added common domain errors

---
+++ FlashCards.Core/Validation/Errors.cs
@@ -0,0 +1,15 @@
+﻿namespace FlashCards.Core.Validation;
+
+public static class Errors
+{
+    public static readonly Error None = Error.None;
+
+    public static readonly Error StackNameRequired = new("StackNameRequired", "Stack name cannot be blank!");
+
+    public static readonly Error StackNameExists = new("StackNameExists", "A stack with that name already exists.");
+
+    public static readonly Error CardFrontTextExists = new("CardFrontTextExists", "A card with that front text already exists.");
+
+    public static readonly Error CardBackTextExists = new("CardBackTextExists", "A card with that back text already exists.");
+
+}
