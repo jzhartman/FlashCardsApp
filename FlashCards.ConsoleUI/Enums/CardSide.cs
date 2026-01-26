782113598c7aab8f03a6c9af15ff8d932be73462
jzhartman <jzhartman@gmail.com>
2026-01-25 23:59:24 -0500

    Beginning to draft enums to eliminate magic strings

---
+++ FlashCards.ConsoleUI/Enums/CardSide.cs
@@ -0,0 +1,8 @@
+﻿namespace FlashCards.ConsoleUI.Enums;
+
+public enum CardSide
+{
+    Front,
+    Back
+}
+
---
+++ FlashCards.ConsoleUI/Enums/MainMenuItem.cs
@@ -0,0 +1,39 @@
+﻿namespace FlashCards.ConsoleUI.Enums;
+
+public enum MainMenuItem
+{
+    Review,
+    Create,
+    Delete,
+    Study,
+    Past,
+    Report,
+    Exit
+}
+
+public static class MainMenuItemExtensions
+{
+    public static string ToFriendlyString(this MainMenuItem menu)
+    {
+        switch (menu)
+        {
+            case MainMenuItem.Review:
+                return "Review Cards in Stack";
+            case MainMenuItem.Create:
+                return "Create New Stack";
+            case MainMenuItem.Delete:
+                return "Delete Stack";
+            case MainMenuItem.Study:
+                return "Begin Study Session";
+            case MainMenuItem.Past:
+                return "View Past Study Sessions";
+            case MainMenuItem.Report:
+                return "View Reports";
+            case MainMenuItem.Exit:
+                return "Exit";
+            default:
+                return "Get your damn dirty hands off me you FILTHY APE!";
+        }
+    }
+}
+
